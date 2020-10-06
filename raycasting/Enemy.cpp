aab506f1d3dfbc7460027f94d8ed0fb8df4f4d70
doomer888 <doomer888@protonmail.com>
2020-10-06 23:10:49 +0300

    added a simple enemy

--- raycasting/Enemy.cpp
+++ raycasting/Enemy.cpp
@@ -1,27 +1,72 @@
 #include "Enemy.h"
+#include "Player.h"
+#include "Map.h"
 
-Enemy::Enemy() {
+sf::CircleShape Enemy::enemyModel;
 
-	pos_x = 100.f;
-	pos_y = 100.f;
-	dx = 0;
-	dy = 0;
-
-	enemyModel = new sf::CircleShape(ENEMY_SIZE);
-	enemyModel->setOrigin(ENEMY_SIZE / 2, ENEMY_SIZE / 2);
-	enemyModel->setFillColor(sf::Color::Red);
-	enemyModel->setPosition(pos_x, pos_y);
+Enemy::Enemy(Map& map, Player& player) :
+	pos_x(100.f),
+	pos_y(100.f),
+	dx(0),
+	dy(0),
+	health(100),
+	map(map),
+	player(player)
+{
+	enemyModel = sf::CircleShape(ENEMY_SIZE);
+	enemyModel.setOrigin(ENEMY_SIZE / 2, ENEMY_SIZE / 2);
+	enemyModel.setFillColor(sf::Color::Red);
+	enemyModel.setPosition(pos_x, pos_y);
 }
 
 Enemy::~Enemy() {
-	delete enemyModel;
 }
 
-void Enemy::update() {
+void Enemy::update(sf::RenderWindow& window, float time) {
+
+	if (isPlayerVisible(player.position())) {
+		if (!checkCollisionWithMap()) {
+			findPath(player.position());
+			pos_x += dx * time; 
+			pos_y += dy * time;
+			enemyModel.setPosition(pos_x, pos_y);
+		}
+	}
+
+	dx = 0;
+	dy = 0; 
 
 }
 
-sf::CircleShape* Enemy::getEnemyModel() {
+void Enemy::findPath(sf::Vector2f destPoint) {
+	float sightAngle = float((atan2(destPoint.y - pos_y, destPoint.x - pos_x)) - M_PI / 6);
+	dx = double(cos(sightAngle)) * SPEED;
+	dy = double(sin(sightAngle)) * SPEED;
+}
+
+bool Enemy::isPlayerVisible(sf::Vector2f destPoint) const {
+	float distance = sqrt((destPoint.x - pos_x) * (destPoint.x - pos_x) + (destPoint.y - pos_y) * (destPoint.y - pos_y));
+
+	return distance < VISIBILITY_RANGE;
+	
+}
+
+bool Enemy::checkCollisionWithMap() const {
+	auto signX = std::copysign(1, dx);
+	auto signY = std::copysign(1, dy);
+
+	return map.checkIntersection(pos_x + ENEMY_SIZE / 2 * signX, pos_y + ENEMY_SIZE / 2 * signY);
+}
+
+float Enemy::size() const {
+	return ENEMY_SIZE;
+}
+
+sf::Vector2f Enemy::position() const {
+	return sf::Vector2f(pos_x, pos_y);
+}
+
+sf::CircleShape& Enemy::getEnemyModel() {
 	return enemyModel;
 }
 
--- raycasting/Enemy.h
+++ raycasting/Enemy.h
@@ -1,25 +1,46 @@
 #ifndef ENEMY_H
 #define ENEMY_H
 
+#define _USE_MATH_DEFINES
+
+#include <math.h>
+
 #include "SFML/Graphics.hpp"
+#include "Map.h"
+
+class Player;
 
 class Enemy {
-private:
-	const float SPEED = 0.01f; 
-	const float ENEMY_SIZE = 10.f; 
-	float pos_x; 
-	float pos_y;
-	float dx; 
-	float dy;
-
-	sf::CircleShape* enemyModel;
-	
 public:
-	Enemy();
+	Enemy(Map&, Player&);
 	~Enemy();
 
-	void update(); 
-	sf::CircleShape* getEnemyModel(); 
+	void update(sf::RenderWindow&, float);
+	void findPath(sf::Vector2f); 
+
+	bool isPlayerVisible(sf::Vector2f) const;
+	bool checkCollisionWithMap() const;
+
+	float size() const;
+	sf::Vector2f position() const;
+	static sf::CircleShape& getEnemyModel(); 
+
+private:
+	const float SPEED = 0.01f;
+	const float ENEMY_SIZE = 10.f;
+	const float VISIBILITY_RANGE = 200.f;
+
+	float pos_x;
+	float pos_y;
+	float dx;
+	float dy;
+
+	float health; 
+
+	Map& map;
+	Player& player;
+
+	static sf::CircleShape enemyModel;
 };
 
 #endif 
